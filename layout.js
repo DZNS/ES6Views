adb52fc5bf8a2a63fb851734ab6859882c616160
Nikhil Nigade <nikhil@dezinezync.com>
2016-02-26 14:10:37 +0530

    updated loading. No more ES5 support.

--- layout.js
+++ layout.js
@@ -1,129 +1,112 @@
 "use strict";
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
+class ModelView {
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; _again = false; if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; desc = parent = undefined; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
+    constructor(data) {
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+        if(data) this.data = data;
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var ModelView = (function () {
-    function ModelView(data) {
-        _classCallCheck(this, ModelView);
-
-        if (data) this.data = data;
     }
 
-    _createClass(ModelView, [{
-        key: "parse",
-        value: function parse() {
+    set data(newData) {
+        this._data = newData;
+        this._markup = undefined;
+    }
 
-            console.log("Subclasses should implement how views are drawn. Do not call super. It does nothing.");
+    get data() {
+        return this._data;
+    }
 
-            return undefined;
-        }
-    }, {
-        key: "data",
-        set: function set(newData) {
-            this._data = newData;
-            this._markup = undefined;
-        },
-        get: function get() {
-            return this._data;
-        }
-    }, {
-        key: "markup",
-        get: function get() {
-            if (!this._markup) this.parse();
+    get markup() {
+        if(!this._markup)
+            this.parse();
 
-            return (this._markup || "").trim();
-        }
-    }, {
-        key: "minified",
-        get: function get() {
-            return ModelView.minify(this.markup);
-        }
-    }], [{
-        key: "minify",
-        value: function minify(html) {
-            return html.replace(/\r?\n?/gim, "").replace(/\s{2,10000}/gim, "");
-        }
-    }]);
+        return (this._markup || "").trim();
+    }
 
-    return ModelView;
-})();
+    get minified() {
+        return ModelView.minify(this.markup);
+    }
 
-var Layout = (function (_ModelView) {
-    _inherits(Layout, _ModelView);
+    parse() {
 
-    function Layout(locals) {
-        _classCallCheck(this, Layout);
+        console.log("Subclasses should implement how views are drawn. Do not call super. It does nothing.");
 
-        _get(Object.getPrototypeOf(Layout.prototype), "constructor", this).call(this, locals || {});
+        return undefined;
 
+    }
+
+    static minify(html) {
+        return html.replace(/\r?\n?/gim, "").replace(/\s{2,10000}/gim,"");
+    }
+
+}
+
+class Layout extends ModelView {
+
+    constructor(locals) {
+
+        super(locals||{});
+        
         this._locals = locals;
 
-        if (!this._locals.renderPartial) this.parse();
+        if(!this._locals.renderPartial)
+            this.parse()
+
     }
 
-    _createClass(Layout, [{
-        key: "parse",
-        value: function parse() {
+    parse() {
 
-            console.log("You should write your common layout logic in a subclass of Layout. When you're done, simply call super with your rendered interstetials.");
+        console.log("You should write your common layout logic in a subclass of Layout. When you're done, simply call super with your rendered interstetials.");
 
-            /*
-             * In your View subclass
-                var header = "...";
-                var main = "...";
-                var footer ="...";
-                super.parse(
-                    header,
-                    main,
-                    footer
-                )
-             */
+        /*
+         * In your View subclass
+            var header = "...";
+            var main = "...";
+            var footer ="...";
+            super.parse(
+                header,
+                main,
+                footer
+            )
+         */
+        
+        /*
+         * In your layout subclass
+        
+            var html = [].slice.call(arguments).join("");
+            this._markup = html;
 
-            /*
-             * In your layout subclass
-            
-                var html = [].slice.call(arguments).join("");
-                this._markup = html;
-             */
-        }
-    }]);
+        */
 
-    return Layout;
-})(ModelView);
+    }
+
+}
 
 function viewEngine(app) {
 
     app.engine('es6', function (filePath, options, callback) {
-
+        
         var partial = options.renderPartial;
         var layout = require(filePath);
         delete options["_locals"]; //circular
 
         var current = new layout(options);
         var env = process.env.NODE_ENV;
-        var isDev = env == "dev" || env == "development";
+        var isDev = (env == "dev" || env == "development");
 
-        if (partial) {
+        if(partial) {
             var markup = current[partial](options);
             callback(undefined, isDev ? markup : ModelView.minify(markup));
             return;
         }
-
+        
         callback(undefined, isDev ? current.markup : current.minified);
+        
     });
+
 }
 
-exports.ModelView = ModelView;
-exports.Layout = Layout;
-exports.viewEngine = viewEngine;
-
+export {ModelView as ModelView}
+export {Layout as Layout}
+export {viewEngine as viewEngine}
